9f50eeec46e3470d323dd2ebe375ff4b3c1d15d6
aditya19970411 <ajain.adityajain@gmail.com>
2019-05-02 10:35:52 +0530

    comments added

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
+//route getting all users with pagination 
 app.get('/users/:pageno/:size', db.getUsers)
-//app.get('/users', db.getUsers)
+//route for getting a friend of a user by id
 app.get('/users/:id', db.getFriend)
+//route for getting a friend of friend by id 
 app.get('/friend/:id', db.getFriendOfFriend)
-//app.post('/users', db.createUser)
-//app.put('/users/:id', db.updateUser)
-//app.delete('/users/:id', db.deleteUser)
+
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
