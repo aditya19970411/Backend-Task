33eab1d5f93211e53cc882c79a72e2d19ba55b50
aditya19970411 <ajain.adityajain@gmail.com>
2019-05-02 08:15:50 +0530

    added queries for friend and friend of friend

--- queries.js
+++ queries.js
@@ -6,19 +6,11 @@ const pool = new Pool({
   password: 'password',
   port: 5432,
 })
-const getUsers = (request, response) => {
-  pool.query('SELECT * FROM users1 ORDER BY id ASC', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
 
 const getFriend = (request, response) => {
   const id = String(request.params.id)
 
-  pool.query('SELECT f2 FROM frienship WHERE f1 = $1', [id], (error, results) => {
+  pool.query('SELECT users1.* from users1 INNER JOIN friend ON users1.id = friend.friend_id WHERE friend.user_id = $1', [id], (error, results) => {
     if (error) {
       throw error
     }
@@ -26,7 +18,31 @@ const getFriend = (request, response) => {
   })
 }
 
+const getUsers = (request, response) => {
+	const limit = parseInt(request.params.size)
+	const offset = (parseInt(request.params.pageno) - 1)*limit
+
+  pool.query('SELECT * FROM users1 ORDER BY id ASC LIMIT $1 OFFSET $2', [limit, offset], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getFriendOfFriend = (request, response) => {
+	const idf = parseInt(request.params.id)
+
+  pool.query(' SELECT u.* FROM users1 u INNER JOIN friend f ON u.id = f.friend_id INNER JOIN friend ff ON f.user_id = ff.friend_id WHERE ff.user_id = $1 AND f.friend_id NOT IN (SELECT friend_id FROM friend WHERE user_id = $1)', [idf], (error, results) => {
+	if (error) {
+		throw error
+	  }
+	response.status(200).json(results.rows)
+  })
+} 
+
 module.exports = {
   getUsers,
-  getFriend
+  getFriend,
+  getFriendOfFriend
   }
